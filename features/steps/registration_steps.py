b6873328dcae0275d369d869195a0ba91c10d9f4
Monther-alkhwaldeh <mmskbh.91@gmail.com>
2024-08-04 10:41:25 +0300

    BBD

---
+++ features/steps/registration_steps.py
@@ -0,0 +1,53 @@
+import time
+
+from behave import *
+
+from Utilites import configReader
+from features.pageObject.RegistrationPage import RegistrationPage
+
+
+@given(u'I Enter The qa.automation.com')
+def step_impl(context):
+    context.reg = RegistrationPage(context.driver)
+    context.reg.open(configReader.readConfig("basic info", "testsiteurl"))
+
+
+@when(u'I enter the name as "{name}"')
+def step_impl(context, name):
+    context.reg.setName(name)
+
+
+@when(u'I enter the phone as "{phone}"')
+def step_impl(context, phone):
+    context.reg.setPhone(phone)
+
+
+@when(u'I Enter the email as "{email}"')
+def step_impl(context, email):
+    context.reg.setEmail(email)
+
+
+@when(u'I enter the country as "{country}"')
+def step_impl(context, country):
+    context.reg.setCountry(country)
+
+
+@when(u'I enter the city as "{city}"')
+def step_impl(context, city):
+    context.reg.setCity(city)
+
+
+@when(u'I enter the username as "{username}"')
+def step_impl(context, username):
+    context.reg.setUsername(username)
+
+
+@when(u'I enter the password as "{password}"')
+def step_impl(context, password):
+    context.reg.setPassword(password)
+
+
+@when(u'I click on the sub,it button')
+def step_impl(context):
+    context.reg.submitForm()
+    time.sleep(2)
